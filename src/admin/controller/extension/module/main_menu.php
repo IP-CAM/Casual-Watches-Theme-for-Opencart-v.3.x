45aca5abfe20dfc8b8589017a1e17d8fdb17f642
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2020-03-28 16:50:35 +0300

    WIP

--- src/admin/controller/extension/module/main_menu.php
+++ src/admin/controller/extension/module/main_menu.php
@@ -34,18 +34,21 @@ class ControllerExtensionModuleMainMenu extends Controller {
 		$data['breadcrumbs'][] = array('text' => $this->language->get('heading_title'), 'href' => $this->url->link('extension/module/custom_main_menu', 'user_token=' . $this->session->data['user_token'], 'SSL'));
 
         //Form action, cancel link
-        $data['action'] = $this->url->link('extension/module/custom_main_menu', 'user_token=' . $this->session->data['user_token'], 'SSL');
+        $data['action'] = $this->url->link('extension/module/main_menu', 'user_token=' . $this->session->data['user_token'], 'SSL');
 		$data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true);
 
         //Data
-        $this->load->model('extension/module/main_menu');
+        //Module status
+        if (isset($this->request->post['main_menu_status'])) {
+            $data['main_menu_status'] = $this->request->post['main_menu_status'];
+        } else {
+            $data['main_menu_status'] = $this->config->get('main_menu_status');
+        }
 
+        $this->load->model('extension/module/main_menu');
         $data['items'] = $this->model_extension_module_main_menu->getItems();
 
-        print_r($data['items']);
-
-        //$data['user_token'] = $this->session->data['user_token'];
-
+        $data['user_token'] = $this->session->data['user_token'];
         $data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
@@ -53,19 +56,31 @@ class ControllerExtensionModuleMainMenu extends Controller {
         $this->response->setOutput($this->load->view('extension/module/main_menu/main_menu_list', $data));
 	}
 
+
     /**
      *
      */
     private function renderItemEditor() {
-        $this->document->setTitle($this->language->get('heading_title'));
 
         //Languages
         $this->load->model('localisation/language');
         $languages = $this->model_localisation_language->getLanguages();
         $data['languages'] = $languages;
 
-        //Form action, cancel link
-        $data['action'] = $this->url->link('extension/module/custom_main_menu/editItem', 'user_token=' . $this->session->data['user_token'] . '&menu_item_id=' . $item_id, true);
+        //Module data
+        if (isset($this->request->get['item_id'])) {
+            $this->load->model('extension/module/main_menu');
+
+            $item_id = $this->request->get['item_id'];
+            $data['item'] = $this->model_extension_module_main_menu->get_item($item_id);
+
+            print_r($data['item']);
+
+            //Form action
+            $data['action'] = $this->url->link('extension/module/main_menu/save_item', 'user_token=' . $this->session->data['user_token'], true);
+        }
+
+        //Cancel link
         $data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=module', true);
 
         //Common controllers
@@ -79,12 +94,20 @@ class ControllerExtensionModuleMainMenu extends Controller {
     /**
      *
      */
-    private function saveItem() {
+    public function save_item() {
+        $this->load->language('extension/module/main_menu');
 
-    }
+        $this->document->setTitle($this->language->get('heading_title'));
 
-    private function updateItem() {
+        $this->load->model('extension/module/main_menu');
 
+        if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+            print_r($this->request->post); //Debug
+
+            if (isset($this->request->post['item-id'])) {
+                print_r($this->model_extension_module_main_menu->create_item($this->request->post));
+            }
+        }
     }
 
     /**
@@ -92,7 +115,7 @@ class ControllerExtensionModuleMainMenu extends Controller {
      */
     public function item_editor() {
         $this->load->language('extension/module/custom_main_menu');
-
+        $this->document->setTitle($this->language->get('heading_title'));
 
 		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
 
@@ -117,27 +140,13 @@ class ControllerExtensionModuleMainMenu extends Controller {
         //...
 
 
-        //Module status
-        if (isset($this->request->post['custom_main_menu_status'])) {
-            $data['custom_main_menu_status'] = $this->request->post['custom_main_menu_status'];
-        } else {
-            $data['custom_main_menu_status'] = $this->config->get('custom_main_menu_status');
-        }
-
-        //Data
-        if (isset($this->request->get['menu_item_id'])) {
-            $item_id = $this->request->get['menu_item_id'];
-        }
-
-        $data['item_id'] = $item_id;
-
 
         //Response
-        $this->renderEditor();
+        $this->renderItemEditor();
     }
 
 	protected function validate() {
-		if (!$this->user->hasPermission('modify', 'extension/module/custom_main_menu')) {
+		if (!$this->user->hasPermission('modify', 'extension/module/main_menu')) {
 			$this->error['warning'] = $this->language->get('error_permission');
 		}
 
--- src/admin/model/extension/module/main_menu.php
+++ src/admin/model/extension/module/main_menu.php
@@ -1,14 +1,49 @@
 <?php
 class ModelExtensionModuleMainMenu extends Model {
 
-    public function getItems() {
+    public function get_items() {
         $result = $this->db->query("SELECT * FROM " . DB_PREFIX ."main_menu");
-
+        return $result->rows;
     }
 
-    public function createItem($data = array()) {
-        $this->db->query("INSERT INTO " . DB_PREFIX . "menu VALUES(" .
-            $data['id']
-        . ")");
+
+    /**
+     *
+     */
+    private function item_decode($item_serialized) {
+        return [
+            'caption' => json_decode($item_serialized['caption'], true),
+            'link' => $item_serialized['link'],
+            'subitems' => json_decode($item_serialized['subitems'], true),
+        ];
+    }
+
+
+
+    public function create_item($data = array()) {
+        $this->db->query("
+            INSERT
+            INTO " . DB_PREFIX . "main_menu(
+                caption,
+                link,
+                subitems
+            )
+            VALUES(
+                '{$this->db->escape(json_encode($data['caption'], true))}',
+                '{$this->db->escape($data['link'])}',
+                '{$this->db->escape(json_encode($data['subitems'], true))}'
+            )
+        ");
+    }
+
+    public function get_item($item_id) {
+
+        $result = $this->db->query("
+            SELECT *
+            FROM " . DB_PREFIX . "main_menu
+            WHERE id=$item_id
+        ");
+
+        return $this->item_decode($result->row);
     }
 }
